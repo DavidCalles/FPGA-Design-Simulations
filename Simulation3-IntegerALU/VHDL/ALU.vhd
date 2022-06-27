89175b4a570d6b14ed3c6def2f89627476b2d14e
David Calles <yodavidcalles@gmail.com>
2022-06-27 16:47:29 -0400

    Initial template for ALU simulation in VHDL

--- Simulation3-IntegerALU/VHDL/ALU.vhd
+++ Simulation3-IntegerALU/VHDL/ALU.vhd
@@ -1,1 +1,25 @@
--- ALU entity
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+entity ALU is
+  port(
+    ABUS : in unsigned(15 downto 0);
+    BBUS : in unsigned(15 downto 0);
+    FSEL : in std_logic_vector(3 downto 0);
+    CIN  : in std_logic;
+
+    C,Z,S,V : out std_logic;
+    FOUT : out unsigned(15 downto 0)
+    );
+end ALU;
+
+architecture RTL of ALU is
+begin
+
+
+end architecture;
